706e34c77913f33574581b49a0efe4959845290b
Allison Bohannon <akboh.github@gmail.com>
2023-10-09 11:29:47 -0700

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div class="p-3 mb-2 bg-dark text-white">
+      <div class="container-fluid">
+          Come solve the mystery of the Ghost of Greenwich! 
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<Home />}/>
+
+          </Routes>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
